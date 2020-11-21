ff99d77ec1123a0a4fd72615627623c33baf7499
Leonel Bazan <leo.bazan.x@gmail.com>
2020-11-21 12:11:20 -0300

    Exercise 2: RNA Transcription (Solution)

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -1,13 +1,11 @@
 defmodule RnaTranscription do
-  @doc """
-  Transcribes a character list representing DNA nucleotides to RNA
+  @next_nucleotide %{?G => ?C, ?C => ?G, ?T => ?A, ?A => ?U}
 
-  ## Examples
-
-  iex> RnaTranscription.to_rna('ACTG')
-  'UGAC'
-  """
-  @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    for x <- dna, do: next_nucleotide(x)
+  end
+
+  defp next_nucleotide(nucleotide) do
+    Map.fetch!(@next_nucleotide, nucleotide)
   end
 end
