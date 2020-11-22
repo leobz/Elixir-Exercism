cb220d67a04f553930a99d5bd82434e950d62882
Leonel Bazan <leo.bazan.x@gmail.com>
2020-11-22 14:26:01 -0300

    Exercise 1.8: Space Age (Solution)

--- space-age/lib/space_age.ex
+++ space-age/lib/space_age.ex
@@ -9,11 +9,25 @@ defmodule SpaceAge do
           | :uranus
           | :neptune
 
+  @eart_year_in_seconds 31557600
+
+  @planet_year_ratio_whit_earth %{
+    :earth => 1,
+    :mercury => 0.2408467,
+    :venus => 0.61519726,
+    :mars => 1.8808158,
+    :jupiter => 11.862615,
+    :saturn => 29.447498,
+    :uranus => 84.016846,
+    :neptune => 164.79132
+  }
+
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet'.
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
+    seconds / (@eart_year_in_seconds * @planet_year_ratio_whit_earth[planet])
   end
 end
